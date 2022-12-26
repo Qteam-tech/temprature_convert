c1b3ad1ba4d79d5295f64fcbb8dca28e686119ed
Mohammad Keshtehgar <mk.qeshmair@gmail.com>
2022-12-26 10:10:29 +0330

    final work but panicked at negetive input

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use std::io;
 
 fn main() {
-    println!("Please Input Temperature Convert Option And Press Enter.");
+    println!("**Please Input Temperature Convert Option And Press Enter**");
     println!("Input 1 To Convert Fahrenheit To Celsius");
     println!("Input 2 To Convert Celsius to Fahrenheit");
     println!("Input 3 To Exit");
-    loop{
 
     let mut option = String::new();
 
@@ -13,22 +12,34 @@ fn main() {
     .read_line(&mut option)
     .expect("Failed To Read Line");
 
-    let option: u32 = option.trim().parse().expect("Please type a number...");
+    let option: u32 = option.trim().parse().expect("Please Input a number...");
     if option == 1 {
         println!("Please input Temprature in Fahrenheit:");
-
+        c_to_f();
     }else if option == 2 {
         println!("Please input Temprature in Celsius:");
+        f_to_c();
     }else if  option ==3 {
-        break;
     }
-    }
+    
 }
 
-fn c_to_f(c:i32)->i32{
-    (c*9/5)+32       
+fn c_to_f(){
+    let mut temp = String::new();
+        io::stdin()
+        .read_line(&mut temp)
+        .expect("Please Input a Number.");
+        let temp:u32=temp.trim().parse().expect("Please Input a number");
+    println!("Temp in Fahrenheit is : {}",(temp*9/5)+32);
+    main();
 }
 
-fn f_to_c(f:i32)->i32{
-    (f-32)*5/9    
+fn f_to_c(){
+    let mut temp = String::new();
+        io::stdin()
+        .read_line(&mut temp)
+        .expect("Please Input a Number.");
+        let temp:u32=temp.trim().parse().expect("Please Input a number");
+    println!("Temp in Celsius is: {}",(temp-32)*5/9);
+    main();
 }
